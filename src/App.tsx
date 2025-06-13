1ac5548b5e80a0c08791be2301e9c3e76495fba5
devsq12395 <seasonquest.mail@gmail.com>
2025-06-14 00:34:32 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 import Sample from './routes/Sample';
 
-const frameworkVersion = '1.1';
+const frameworkVersion = '1.2';
 
 function AppRoutes() {
   console.log('Created with SQ Framework Version:', frameworkVersion);
